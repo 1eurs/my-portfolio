7202dc9d83c462e6c7165e04916e219628e014be
1eurs <Mahmoodazleidi@hotmail.com>
2022-05-26 15:20:02 +0200

    adding About Page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,7 @@
 const Projects = (props) => {
   return (
     <div className="mt-40 mob:mt-10 laptop:mt-40 mob:p-2 laptop:p-0">
-      <h1>Projects.</h1>
+      <h1 className="text-2xl text-bold">Projects.</h1>
       <div className="mt-10 mob:mt-5 laptop:mt-10 grid grid-cols-2 mob:grid-cols-1 laptop:grid-cols-2 gap-4">
         {props.children}
       </div>
